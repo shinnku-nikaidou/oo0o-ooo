0685250893aedc7a82aa5169ea3c8e5883be6e8c
Shinnku <nikaidou@shinnku.com>
2025-07-22 19:48:16 +0800

    Add ESLint configuration with support for React, TypeScript, Astro, and Prettier
    - Set up ESLint with recommended configurations for React, React Hooks, TypeScript, and Astro.
    - Integrate Prettier as a plugin to enforce code formatting rules.
    - Configure parsers for TypeScript and Astro files.
    - Specify ignored directories to exclude from linting.

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+import tsParser from "@typescript-eslint/parser";
+import astroParser from "astro-eslint-parser";
+
+const compat = new FlatCompat({
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  ...compat.extends(
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended"
+  ),
+  {
+    plugins: { prettier },
+    rules: { "prettier/prettier": "error" }
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+      },
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { sourceType: "module" },
+    },
+  },
+  {
+    ignores: ["dist/**", ".astro/**", "node_modules/**"],
+  },
+];
